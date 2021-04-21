9fd6140c55791d5e4ff202dc405abec592688518
Aiden He <aidenhe@outlook.com>
2021-04-21 16:59:29 +0800

    refresh project

---
+++ code/Weibo2/app/src/main/java/com/practice/weibo2/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.practice.weibo2.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
